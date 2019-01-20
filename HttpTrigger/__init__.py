8799d160157515a53bf799bc6235d111fcd1a24c
Jordan <jordan@eremieff.com>
2019-01-20 18:06:25 +1100

    Update app example using Mangum adapter

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -1,11 +1,26 @@
 import logging
-
 import azure.functions as func
-from .asgi import asgi_handler
-from .app import App
+from mangum.handlers.azure import azure_handler
+
+
+class App:
+    def __init__(self, scope) -> None:
+        self.scope = scope
+
+    async def __call__(self, receive, send) -> None:
+        message = await receive()
+        if message["type"] == "http.request":
+            await send(
+                {
+                    "type": "http.response.start",
+                    "status": 200,
+                    "headers": [[b"content-type", b"text/plain"]],
+                }
+            )
+            await send({"type": "http.response.body", "body": b"Hello, world!"})
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
-    response = asgi_handler(App, req)
+    response = azure_handler(App, req)
     return func.HttpResponse(response["body"], status_code=response["status_code"])
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # azure-functions-python-asgi-example
 
-An example [ASGI](https://asgi.readthedocs.io/en/latest/) application adapted to [Azure Functions](https://azure.microsoft.com/en-au/services/functions/).
+An example [ASGI](https://asgi.readthedocs.io/en/latest/) application adapted to [Azure Functions](https://azure.microsoft.com/en-au/services/functions/) using the Azure Function adapter in [Mangum](https://github.com/erm/mangum).
+
+### Reference
 
 https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-python
+
 https://docs.microsoft.com/en-us/python/api/azure-functions/azure.functions?view=azure-python
